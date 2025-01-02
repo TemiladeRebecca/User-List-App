052d49e7c8b0b913337169dbe60d1798297a979e
TemiladeRebecca <temilade.rebecca20@gmail.com>
2025-01-02 23:55:35 +0100

    feat: display a single user's full profile

---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserDetails from '../components/UserDetails';
+
+const DetailsPage = () => {
+  return (
+    <div className="details-page">
+      <UserDetails />
+    </div>
+  );
+};
+
+export default DetailsPage;
