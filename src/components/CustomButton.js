49c35bab4ee055e6f03bef7b4b34b8275a68d55e
TemiladeRebecca <temilade.rebecca20@gmail.com>
2025-01-03 14:00:39 +0100

    chore: improve layout of icons and button in CustomButton component

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './CustomButton.module.css'
+import classes from './CustomButton.module.css';
 
 const CustomButton = ({ isDarkMode, toggleTheme }) => {
   return (
