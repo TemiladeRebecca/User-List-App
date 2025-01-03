6b4ab8e608f9fbaaec704febbccefcef1d8517c1
TemiladeRebecca <temilade.rebecca20@gmail.com>
2025-01-03 20:13:58 +0100

    chore: update heading text from 'Users' to 'User List' for better clarity

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -18,7 +18,7 @@ const UserList = () => {
     
     return (
         <div className={classes.userList}>
-          <h1>Users</h1>
+          <h1>User List</h1>
           <ul>
             {users.map(user => (
               <li key={user.id} className={classes.text}>
